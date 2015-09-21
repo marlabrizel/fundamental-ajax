b6980a0e164b47bfe1874974bf8914c09ea346dd
Marla Brizel <marlabrizel@gmail.com>
2015-09-21 10:12:15 -0600

    add ajax call to record a new post

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -23,13 +23,22 @@ $(document).ready(function(){
         description: $("#post-description").val()
       }
     }
-    console.log(postParams);
+
+    $.ajax({
+      type:    "POST",
+      url:     "https://turing-birdie.herokuapp.com/api/v1/posts.json",
+      data:    postParams,
+      success: function(post){
+        $("#latest-posts").append(
+          "<div class='post' data-id='"
+          + post.id
+          + "'><h6>Published on"
+          + post.created_at
+          + "</h6><p>"
+          + post.description
+          + "</p></div>"
+        )
+      }
+    });
   });
-  // var postParams =
-  //
-  // $.ajax({
-  //   type: "POST",
-  //   url: "https://turing-birdie.herokuapp.com/api/v1/posts.json",
-  //   data: postParams
-  // });
 });
