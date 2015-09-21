8d66e050f752004a474013a76d281aaaf8774cda
Marla Brizel <marlabrizel@gmail.com>
2015-09-21 10:25:55 -0600

    modify fetch posts to listen for button click

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -1,20 +1,22 @@
 $(document).ready(function(){
-  $.ajax({
-    type: "GET",
-    url: "https://turing-birdie.herokuapp.com/api/v1/posts.json",
-    success: function(posts) {
-      $.each(posts, function(index, post){
-        $("#latest-posts").append(
-          "<div class='post' data-id='"
-          + post.id
-          + "'><h6>Published on"
-          + post.created_at
-          + "</h6><p>"
-          + post.description
-          + "</p></div>"
-        )
-      });
-    }
+  $("#fetch-posts").on("click", function (){
+    $.ajax({
+      type: "GET",
+      url: "https://turing-birdie.herokuapp.com/api/v1/posts.json",
+      success: function(posts) {
+        $.each(posts, function(index, post){
+          $("#latest-posts").append(
+            "<div class='post' data-id='"
+            + post.id
+            + "'><h6>Published on"
+            + post.created_at
+            + "</h6><p>"
+            + post.description
+            + "</p></div>"
+          )
+        });
+      }
+    });
   });
 
   $("#create-post").on("click", function (){
