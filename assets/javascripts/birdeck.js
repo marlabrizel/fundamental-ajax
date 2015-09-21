22b3c3c5dbac026ace981ff6a50aaef05187d3f5
Marla Brizel <marlabrizel@gmail.com>
2015-09-21 11:12:38 -0600

    conditional for post rendering
    if the post is newer than the newest item on the DOM, then render the post
    check for NaN condition

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -17,7 +17,9 @@ function fetchPostsButton (){
       url:     "https://turing-birdie.herokuapp.com/api/v1/posts.json",
       success: function(posts) {
         $.each(posts, function(index, post){
-          renderPost(post)
+          if (isNaN(newestItemID) || post.id > newestItemID) {
+            renderPost(post)
+          }
         })
       }
     })
